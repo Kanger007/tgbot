a905d0a1ec4b7366a18b5f529d5cc2f59eebdbc8
Vito Corlene <41597046+AnonymousKiddo@users.noreply.github.com>
2018-07-24 22:10:29 +0530

    Update warns.py

--- tg_bot/modules/warns.py
+++ tg_bot/modules/warns.py
@@ -63,12 +63,12 @@ def warn(user: User, chat: Chat, reason: str, message: Message, warner: User = N
 
     else:
         keyboard = InlineKeyboardMarkup(
-            [[InlineKeyboardButton("Remove warn", callback_data="rm_warn({})".format(user.id))]])
+            [[InlineKeyboardButton("Cancel ❌", callback_data="rm_warn({})".format(user.id))]])
 
-        reply = "{} has got {} out of {} warnings... 🚫!".format(mention_html(user.id, user.first_name), num_warns,
+        reply = "{} has warned {} of {} ⚠️!".format(mention_html(user.id, user.first_name), num_warns,
                                                              limit)
         if reason:
-            reply += "\nReason for last warn:\n{}".format(html.escape(reason))
+            reply += "\nReason :\n{}".format(html.escape(reason))
 
         log_reason = "<b>{}:</b>" \
                      "\n#WARN" \
