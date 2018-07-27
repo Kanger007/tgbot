31c8d42a2fed81d2f5f655d318751acc7af58228
Vito Corlene <41597046+AnonymousKiddo@users.noreply.github.com>
2018-07-27 16:24:28 +0530

    Update global_bans.py

--- tg_bot/modules/global_bans.py
+++ tg_bot/modules/global_bans.py
@@ -147,7 +147,7 @@ def ungban(bot: Bot, update: Update, args: List[str]):
 
     banner = update.effective_user  # type: Optional[User]
 
-    message.reply_text("I'm gonna make him an offer that {} can't refuse !".format(user_chat.first_name))
+    message.reply_text("I'm gonna make {} an offer that he can't refuse !".format(user_chat.first_name))
 
     send_to_list(bot, SUDO_USERS + SUPPORT_USERS,
                  "{} has ungbanned user {}".format(mention_html(banner.id, banner.first_name),
