023434a3d2743cc8e9b1dd30437feb48b5f572b1
Kanger007 <41924629+Kanger007@users.noreply.github.com>
2018-07-31 00:31:23 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -142,7 +142,7 @@ def start(bot: Bot, update: Update, args: List[str]):
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
                 parse_mode=ParseMode.MARKDOWN)
     else:
-        update.effective_message.reply_text("""Hey there, i'm alive """)
+        update.effective_message.reply_text("""Hey there, i'm alive 😉 """)
 
 
 # for test purposes
